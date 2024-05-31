f5db7d87643b0d5dd2caefcdba07962ccff6c96b
alkachhirang <alkachhirang3@gmail.com>
2024-05-31 11:40:15 +0530

    carddata

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,19 @@
+import { feedCarddata } from "./common/Helper";
+
 export default function Feed() {
-    return(
-        <div></div>
+    return (
+        <div className=''>
+            {feedCarddata.map((data, index) => {
+                return (
+                    <div key={index} className="flex flex-col items-center justify-center">
+                        <div>
+                            <div>{data.Svg}</div>
+                            <p className="max-w-[186px] leading-[120%]">{data.content}</p>
+                        </div>
+
+                    </div>
+                );
+            })}
+        </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
+import Feed from "@/components/Feed";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <div className='bg-lightGreen max-w-[393px] mx-auto px-4'>
+       <Feed/>
+
+      </div>
 
     </>
   );
