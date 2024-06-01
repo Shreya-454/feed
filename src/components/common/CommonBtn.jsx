9d5597c69787181242e2f24ac9742528f23cd4c3
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-01 09:28:08 +0530

    updates

--- src/components/common/CommonBtn.jsx
+++ src/components/common/CommonBtn.jsx
@@ -5,7 +5,7 @@ const CommonBtn = ({ text, icon, className, gap, type, ...props }) => {
 
   return (
     <button
-      className={`flex items-center gap-5 bg-lightGreen leading-1lg shadow-[1px_1px_0px_0px_#0C0A0980] w-[100px] h-[34px] border-[#166534] text-xs tracking-[0.01em] px-[12px] py-[9.5px] border border-solid rounded-[4px] transition-all duration-300 ease-linear ${className}`}
+      className={`flex items-center gap-5 bg-lightGreen leading-1lg shadow-[1px_1px_0px_0px_#0C0A0980] w-[100px] h-[34px] border-[#166534] text-xs tracking-[0.01em] px-[12px] py-[9.5px] border border-solid rounded-[6px] transition-all duration-300 ease-linear ${className}`}
 
      
     >
