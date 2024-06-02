ff998e8acc98dfebd38c362d46bf36b18a0ca3de
alkachhirang <alkachhirang3@gmail.com>
2024-06-02 08:42:36 +0530

    popup-add

--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -138,35 +138,51 @@ export const basedCarddata = [
         icon: '/assets/svg/time.svg',
         text: '23m 15s',
     },
-    {digit: "#1",
-    content1: '1295059.36',
-    icon: '/assets/svg/shield.svg',
-    text: 'Shield',
+    {
+        digit: "#1",
+        content1: '1295059.36',
+        icon: '/assets/svg/shield.svg',
+        text: 'Shield',
 
-},
-{
-    digit: "#1",
-    content1: '1295059.36',
-    icon: '/assets/svg/bonk.svg',
-    text: 'Bonk',
-},
-{
-    digit: "#1",
-    content1: '1295059.36',
-    icon: '/assets/svg/time.svg',
-    text: '23m 15s',
+    },
+    {
+        digit: "#1",
+        content1: '1295059.36',
+        icon: '/assets/svg/bonk.svg',
+        text: 'Bonk',
+    },
+    {
+        digit: "#1",
+        content1: '1295059.36',
+        icon: '/assets/svg/time.svg',
+        text: '23m 15s',
 
-},
-{
-    digit: "#1",
-    content1: '1295059.36',
-    icon: '/assets/svg/shield.svg',
-    text: 'Shield',
-},
-{
-    digit: "#1",
-    content1: '1295059.36',
-    icon: '/assets/svg/shield.svg',
-    text: 'Shield',
-},
+    },
+    {
+        digit: "#1",
+        content1: '1295059.36',
+        icon: '/assets/svg/shield.svg',
+        text: 'Shield',
+    },
+    {
+        digit: "#1",
+        content1: '1295059.36',
+        icon: '/assets/svg/shield.svg',
+        text: 'Shield',
+    },
+]
+export const shieldBtnpopup = [
+    {
+        heading: "Asphyxiated",
+        para: "11",
+        para2: "10",
+        para3: "2d 2h 24m 30s",
+    },
+]
+export const BonkBtnpopup = [
+    {
+        heading: "Rules",
+        para: "You can attack a pet above your level every 30mins and earn 0.5% of their rewards.",
+        para2: "A pet can only be attacked once per hour.",
+    },
 ]
