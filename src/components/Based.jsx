e75093a78ef8fea76ca769fa647349ca62fa8e22
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-03 13:48:56 +0530

    btn hover

--- src/components/Based.jsx
+++ src/components/Based.jsx
@@ -74,7 +74,7 @@ const Based = () => {
              {isModalOpen && (
                 <div className='fixed inset-0 backdrop-blur-[10px] max-w-[393px] bg-[#9ED2AECC] mx-auto flex items-center justify-center z-50 left-0 top-0'>
                     <div className='bg-lightGreen rounded-[16px] p-2 max-w-[361px] w-full mx-auto overflow-hidden'>
-                        <div className='max-w-[344px] rounded-lg p-4 bg-[#F0FDF4]'>
+                        <div className='max-w-[344px] rounded-lg p-4 bg-offWhite'>
                             <div className=' flex items-center gap-2 mb-[18px]'>
                             <Image src="/assets/svg/popup1.svg" alt="popupsvg" width={22} height={28}/>
                                 <span className=' font-bold text-base leading-5 text-darkGreen'>Asphyxiated</span>
--- src/components/common/CommonBtn.jsx
+++ src/components/common/CommonBtn.jsx
@@ -6,7 +6,7 @@ const CommonBtn = ({ text, icon, className, ...props }) => {
   return (
     <button
       className={`flex items-center gap-5 bg-lightGreen leading-1lg shadow-[1px_1px_0px_0px_#16653480] 
-      w-[100px] h-[34px] border-darkGreen hover:bg-[#16653480] hover:shadow-none hover:text-black text-xs tracking-[0.01em] px-[12px] py-[9.5px] border border-solid rounded-[6px] transition-all duration-300 ease-linear ${className}`}
+      w-[100px] h-[34px] border-darkGreen  hover:shadow-none hover:text-black text-xs tracking-[0.01em] px-[12px] py-[9.5px] border border-solid rounded-[6px] transition-all duration-300 ease-linear ${className}`}
       {...props}
     >
       {icon && <Image src={icon} alt={`${text} icon`} width={16} height={16} />}
