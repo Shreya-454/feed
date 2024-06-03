204c687ce36e336a0fbed713fca2cc6a32d8f92c
Shreya-454 <bshreya932@GMAIL.COM>
2024-06-03 09:31:09 +0530

    border

--- src/components/Based.jsx
+++ src/components/Based.jsx
@@ -81,7 +81,7 @@ const Based = () => {
           
 <label className="inline-flex items-center cursor-pointer">
   <input type="checkbox" value="" className="sr-only peer"/>
-  <div className="relative w-8 h-4 bg-gray-200 peer-focus:outline-none outline-none border-none rounded-full peer dark:bg-Green peer-checked:after:translate-x-0 after:translate-x-[193%] peer-checked:after:border-white after:content-[''] after:absolute after:top-[3.5px] after:start-[2px] after:bg-[#DCFCE7] shadow-[inset_2px_2px_0px_0px_#0C0A0980] peer-checked:shadow-[1px_1px_0px_0px_#16653480] duration-300
+  <div className="relative w-8 h-4 bg-gray-200 peer-focus:outline-none outline-none border-none rounded-full peer dark:bg-Green peer-checked:after:translate-x-0 after:translate-x-[193%] after:content-[''] after:absolute after:top-[3.5px] after:start-[2px] after:bg-[#DCFCE7] shadow-[inset_2px_2px_0px_0px_#0C0A0980] peer-checked:shadow-[1px_1px_0px_0px_#16653480] duration-300
                         peer-checked:after:bg-Green peer-checked:after:shadow-[inset_2px_2px_0px_0px_#0C0A0980] after:rounded-full after:h-[9.6px] after:w-[9.6px] after:transition-all peer-checked:bg-[#F0FDF4]"></div>
   <span className="text-xs tracking-[0.01em] font-normal text-darkGreen pl-[6px] leading-1lg">Show Bonkable (32)</span>
 </label>
